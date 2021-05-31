343c2724553a1f0bad93aca671a73da300a189f4
Pablo Gómez <pablogomez@pablogomez.com>
2021-05-31 08:44:17 +0100

    Committing fixes in the dark, seriously, who killed my power!?

--- config.def.h
+++ config.def.h
@@ -12,9 +12,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Font Awesome 5 Free:size=8",
+static const char *fonts[]          = { "Font Awesome 5 Free:size=9",
                                         "monospace:size:30" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "Font Awesome 5 Free:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
