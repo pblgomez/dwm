6b267077f2062c8468389bd0f0cb5701147824e4
Pablo Gómez <pablogomez@pablogomez.com>
2021-06-02 08:13:55 +0100

    this is my quickfix branch and i will use to do my quickfixes

--- config.def.h
+++ config.def.h
@@ -71,7 +71,7 @@ static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	//{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,			                  XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
